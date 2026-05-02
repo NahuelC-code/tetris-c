b8983c24e631262651965baef06653507f58a4e3
Nahuel Cavallaro <nahuelcavallaro@gmail.com>
2026-05-02 01:58:18 -0300

    Implementacion funcion mostrar_tablero durante ejecucion

--- game.h
+++ game.h
@@ -10,4 +10,5 @@
 int valida_spawn_pieza(int board[][COL_BOARD],Tetrimino* p);
 int valida_shift_down_pieza(int board[][COL_BOARD], Tetrimino* p);
 void colocar_pieza(int board[][COL_BOARD],Tetrimino* p) ;
+void mostrar_tablero(int board[][COL_BOARD],Tetrimino* p) ;
 #endif // GAME_H_INCLUDED
